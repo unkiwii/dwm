8c655624a835988fe211b580850ef283e35215af
Lucas G. Sánchez <unkiwii@gmail.com>
2025-06-23 00:41:05 -0300

    Back to showing bar by default

--- config.def.h
+++ config.def.h
@@ -38,7 +38,7 @@ static const unsigned int systrayonleft = 1;    /* 0: systray in the right corne
 static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray        = 1;        /* 0 means no systray */
-static const int showbar            = 0;        /* 0 means no bar */
+static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Go Mono:size=10", "Inconsolata Nerd Font Mono:size=20" };
 static const char dmenufont[]       = "Inconsolata Nerd Font Mono:size=24";
